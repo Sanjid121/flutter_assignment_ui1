dd97ed3423ed229f35b1b383a0d3ea0eec0ca6a2
Git RX <95379583+moniroul@users.noreply.github.com>
2024-10-23 20:58:38 +0600

    add by r

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_assignment_ui/main_page.dart';
-
+// add by russell
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
